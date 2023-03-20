d7ec5f72ed7132173a797778fbbd2ed7e72b49fd
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-03-20 15:22:00 +0530

    minor change

--- src/OtelHook.php
+++ src/OtelHook.php
@@ -8,23 +8,47 @@
 declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
+
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\HttpFactory;
 use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
 use OpenTelemetry\Contrib\Otlp\SpanExporter;
-use OpenTelemetry\SDK\Trace\SpanExporter\ConsoleSpanExporter;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
-use OpenTelemetry\SDK\Trace\TracerProvider;
 
-\OpenTelemetry\Instrumentation\hook(
+use OpenTelemetry\SDK\Trace\TracerProvider;
+use OpenTelemetry\SDK\Common\Instrumentation As InstrumentationLibrary;
+
+//putenv('OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:9321/v1/traces');
+//putenv('OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf');
+putenv('OTEL_PHP_AUTOLOAD_ENABLED=true');
+putenv('OTEL_SERVICE_NAME=mw-php-app');
+
+echo 'Starting CollectorSpanExporter' . PHP_EOL;
+
+$transport = (new OtlpHttpTransportFactory())->create('http://localhost:9321/v1/traces', 'application/x-protobuf');
+$exporter = new SpanExporter($transport);
+
+$tracerProvider = new TracerProvider(
+    new SimpleSpanProcessor(
+        $exporter
+    )
+);
+
+$tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php');
+InstrumentationLibrary\hook(
     DemoClass::class,
     'run',
     static function (DemoClass $demo, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($tracer) {
-        static $instrumentation;
+        /*static $instrumentation;
         $instrumentation ??= new CachedInstrumentation('example');
         $instrumentation->tracer()->spanBuilder($class)
             ->startSpan()
+            ->activate();*/
+
+        $span = $tracer->spanBuilder($class)
+            ->startSpan()
             ->activate();
+        Context::storage()->attach($span->storeInContext(Context::getCurrent()));
     },
     static function (DemoClass $demo, array $params, $returnValue, ?Throwable $exception) use ($tracer) {
         $scope = Context::storage()->scope();
